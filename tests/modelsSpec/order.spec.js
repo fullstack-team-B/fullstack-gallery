94a287e05fb2c5cfd4dfead4650755cc698c3708
Javier Montero <monterodotjavier@icloud.com>
2020-06-22 23:31:44 -0400

    spec: order model

---
+++ tests/modelsSpec/order.spec.js
@@ -0,0 +1,40 @@
+const {expect} = require('chai')
+const db = require('../../server/db/index')
+const Order = db.model('order')
+
+describe('Order model', async () => {
+  beforeEach(() => {
+    return db.sync({force: true})
+  })
+
+  let order
+  let date = new Date().toDateString()
+
+  beforeEach(() => {
+    order = Order.build({
+      orderPlaced: date,
+      firstName: 'Jeff',
+      lastName: 'Bozo',
+      completed: false,
+      paymentUsed: null,
+      price: 25
+    })
+  })
+
+  afterEach(() => {
+    return Promise.all([Order.truncate({cascade: true})])
+  })
+
+  describe('Type Validation', () => {
+    it('includes `orderPlaced`, `firstName` `lastName`, `completed`, paymentUsed, and price', async () => {
+      const savedOrder = await order.save()
+
+      expect(savedOrder.orderPlaced.toDateString()).to.equal(date)
+      expect(savedOrder.firstName).to.equal('Jeff')
+      expect(savedOrder.lastName).to.equal('Bozo')
+      expect(savedOrder.completed).to.equal(false)
+      expect(savedOrder.paymentUsed).to.equal(null)
+      expect(savedOrder.price).to.equal(25)
+    })
+  })
+})
